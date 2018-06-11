a69124130eacd25f21a7bce1ee88f4055b350f5e
Nick <ntelfer40@gmail.com>
2018-06-11 14:20:45 -0600

    updates to the cli and ioflo behaviors

--- src/pydidery/cli.py
+++ src/pydidery/cli.py
@@ -19,28 +19,30 @@ import click
 import ioflo.app.run
 
 from ioflo.aid import odict
-from ioflo.aid.consoling import VERBIAGE_NAMES
 
 
 """
 Command line interface for didery.py library.  Path to config file containing server list required
 """
 @click.command()
-@click.argument(
-    'config',
-    type=click.File(),
+@click.option(
+    '--upload',
+    multiple=False,
+    type=click.Choice(['otp', 'history']),
+    help='Choose the type of save for existing keys.'
 )
 @click.option(
-    '--save',
+    '--rotate',
     multiple=False,
-    type=click.Choice(['create-otp', 'update-otp', 'create-history', 'update-history']),
-    help='choose the type of save'
+    is_flag=True,
+    default=False,
+    help='Send rotation event to didery servers.'
 )
 @click.option(
     '--retrieve',
     multiple=False,
     type=click.Choice(['otp', 'history']),
-    help='retrieve otp or history data'
+    help='Retrieve otp or history data.'
 )
 @click.option(
     '--data',
@@ -48,7 +50,7 @@ Command line interface for didery.py library.  Path to config file containing se
     multiple=False,
     default=None,
     type=click.Path(exists=True, file_okay=True, dir_okay=False, readable=True, resolve_path=True),
-    help='path to data file'
+    help='Path to data file.'
 )
 @click.option(
     '--consensus',
@@ -56,19 +58,27 @@ Command line interface for didery.py library.  Path to config file containing se
     multiple=False,
     default=50,
     type=click.IntRange(0, 100),
-    help='threshold(%) at which consensus is reached'
+    help='Threshold(%) at which consensus is reached.'
 )
 @click.option(
     '-v',
     multiple=False,
     count=True,
-    help='verbosity of console output'
+    help='Verbosity of console output. There are 4 verbosity levels -vvvv.'
 )
-def main(config, save, retrieve, data, consensus, v):
-    verbose = v
+@click.argument(
+    'config',
+    type=click.File(),
+)
+def main(upload, rotate, retrieve, data, consensus, v, config):
+    verbose = v if v <= 4 else 4
 
-    if save and data is None:
-        click.echo('data file required to save. Use --data, -d [file path]')
+    if upload == 'otp' and data is None:
+        click.echo('data file required to upload otp blobs. Use --data, -d [file path]')
+        return
+
+    if rotate and data is None:
+        click.echo('data file required to start rotation event. Use --data, -d [file path]')
         return
 
     projectDirpath = os.path.dirname(
@@ -81,10 +91,8 @@ def main(config, save, retrieve, data, consensus, v):
     floScriptpath = os.path.join(projectDirpath, "pydidery/flo/main.flo")
 
     """ Main entry point for ioserve CLI"""
-    if verbose > 4:
-        verbose = 4
 
-    ioflo.app.run.run(  name="skedder",
+    ioflo.app.run.run(  name="didery.py",
                         period=0.125,
                         real=True,
                         retro=True,
--- src/pydidery/core/behaving.py
+++ src/pydidery/core/behaving.py
@@ -1,7 +1,21 @@
+from ioflo.aid import getConsole
 from ioflo.aid import odict
 from ioflo.base import doify
 
 
-@doify('CommandProcessing', ioinits=odict(test=""))
-def commandProcessing(self):
+console = getConsole()
+
+
+@doify('UploadData', ioinits=odict(test=""))
+def uploadData(self):
+    console.terse("UPLOAD!")
+
+
+@doify('Rotation', ioinits=odict(test=""))
+def rotation(self):
+    pass
+
+
+@doify('Retrieval', ioinits=odict(test=""))
+def retrieval(self):
     pass
